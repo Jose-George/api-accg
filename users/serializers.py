6017216caa9fbbe0983b5ec7d48956641e2318a7
Kaymmi Nunes <kaymmiknb@gmail.com>
2026-07-19 15:24:26 -0300

    fix(users): restringe gerenciamento de usuários a administradores

--- users/serializers.py
+++ users/serializers.py
@@ -1,28 +1,69 @@
 from rest_framework import serializers
+
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'username', 'password']
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_active",
+            "password",
+        ]
+        read_only_fields = [
+            "id",
+        ]
         extra_kwargs = {
-            'password': {'write_only': True}
+            "password": {
+                "write_only": True,
+                "required": False,
+                "min_length": 8,
+            },
         }
 
+    def validate(self, attrs):
+        if (
+            self.instance is None
+            and not attrs.get("password")
+        ):
+            raise serializers.ValidationError(
+                {
+                    "password": (
+                        "A senha é obrigatória para "
+                        "criar um usuário."
+                    )
+                }
+            )
+
+        return attrs
+
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        user = User(**validated_data)
-        if password:
-            user.set_password(password)
-        user.save()
+        password = validated_data.pop("password")
+
+        user = User.objects.create_user(
+            password=password,
+            **validated_data,
+        )
+
         return user
 
     def update(self, instance, validated_data):
-        password = validated_data.pop('password', None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
+        password = validated_data.pop(
+            "password",
+            None,
+        )
+
+        for attribute, value in validated_data.items():
+            setattr(instance, attribute, value)
+
         if password:
             instance.set_password(password)
+
         instance.save()
+
         return instance
